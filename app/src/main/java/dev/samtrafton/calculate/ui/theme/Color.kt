1a99f0b7e5f8e86252a3b406afa4163d6cc02309
Sam Trafton <samatrafton@gmail.com>
2024-09-15 07:36:17 +0300

    Initial commit

---
+++ app/src/main/java/dev/samtrafton/calculate/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package dev.samtrafton.calculate.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF1A0350)
+val PurpleGrey80 = Color(0xFF110626)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF880505)
+val PurpleGrey40 = Color(0xFF2F0093)
+val Pink40 = Color(0xFF7D5260)
